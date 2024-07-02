b84a144d1839711575e39e72c46de77ddcc5efed
sullis <github@seansullivan.com>
2024-07-02 04:29:46 -0700

    migrate from junit 4 to junit 5

--- src/test/java/org/webjars/WebJarVersionLocatorTest.java
+++ src/test/java/org/webjars/WebJarVersionLocatorTest.java
@@ -1,50 +1,50 @@
 package org.webjars;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.jspecify.annotations.NullMarked;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
-public class WebJarVersionLocatorTest {
+class WebJarVersionLocatorTest {
 
     @Test
-    public void invalid_webjar_path_should_return_null() {
+    void invalid_webjar_path_should_return_null() {
         assertNull(new WebJarVersionLocator().version("foo"));
     }
 
     @Test
-    public void should_get_a_webjar_version() {
+    void should_get_a_webjar_version() {
         assertEquals("3.1.1", new WebJarVersionLocator().version("bootswatch-yeti"));
     }
 
     @Test
-    public void webjar_version_doesnt_match_path() {
+    void webjar_version_doesnt_match_path() {
         assertEquals("3.1.1", new WebJarVersionLocator().version("bootstrap"));
     }
 
     @Test
-    public void full_path_exists_version_not_supplied() {
+    void full_path_exists_version_not_supplied() {
         assertEquals(WebJarVersionLocator.WEBJARS_PATH_PREFIX + "/bootstrap/3.1.1/js/bootstrap.js", new WebJarVersionLocator().fullPath("bootstrap", "js/bootstrap.js"));
     }
 
     @Test
-    public void path_exists_version_not_supplied() {
+    void path_exists_version_not_supplied() {
         assertEquals("bootstrap/3.1.1/js/bootstrap.js", new WebJarVersionLocator().path("bootstrap", "js/bootstrap.js"));
     }
 
     @Test
-    public void full_path_exists_version_supplied() {
+    void full_path_exists_version_supplied() {
         assertEquals(WebJarVersionLocator.WEBJARS_PATH_PREFIX + "/bootstrap/3.1.1/js/bootstrap.js", new WebJarVersionLocator().fullPath("bootstrap", "3.1.1/js/bootstrap.js"));
     }
 
     @Test
-    public void cache_is_populated_on_lookup() {
+    void cache_is_populated_on_lookup() {
         AtomicInteger numLookups = new AtomicInteger(0);
 
         @NullMarked
